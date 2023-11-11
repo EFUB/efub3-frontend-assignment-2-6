39513cf38757c12e04cce20781b857de6771903a
teyeong <taetae0202@naver.com>
2023-11-11 18:36:06 +0900

    fix: 미사용 코드 삭제

--- app/components/RecordWrite.js
+++ app/components/RecordWrite.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useRef, useCallback, useEffect } from "react";
+import { useState, useRef } from "react";
 import styles from "./RecordWrite.module.css";
 
 export default function RecordWrite({ itemList, setItemList }) {
